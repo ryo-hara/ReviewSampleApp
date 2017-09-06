8be2b4c55ff59e33d08add69d8ab0366a65b6625
ryo_hara_ <akstkr@gmail.com>
2017-09-06 03:54:56 +0000

    add: 6.2終わりまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    before_save { email.downcase! }
+    VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :name,    presence: true, length: {maximum: 50,   minimum: 2}
+    validates :email,   presence: true, length: {maximum: 255,  minimum: 2}
+    validates :email,   format:{with: VALID_EMAIL_REGAX},   uniqueness: { case_sensitive: false }
+
 end
